e142a8e0744964e232a57b214a517d4e8ab69a90
Dhruv Gupta <dhruv.24bce10799@vitbhopal.ac.in>
2026-07-22 20:47:09 +0530

    Create football.cpp
    Day 13 - Football

---
+++ football.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+	// your code goes here
+
+string s;
+cin>>s;
+int count=0;
+for(int i=0;i<s.size();i++){
+    if(s[i]==s[i+1]){
+        count++;
+    }else{
+        count=0;
+    }
+    if(count>=6){
+        cout<<"YES";
+        return 0;
+    }
+}cout<<"NO";
+}
