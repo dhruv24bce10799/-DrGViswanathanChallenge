4224eef47510f67d2c997375d6820c593d0a9069
Dhruv Gupta <dhruv.24bce10799@vitbhopal.ac.in>
2026-07-22 20:45:13 +0530

    Create stonesOnTheTable.cpp
    Day 12 - Stones On The Table

---
+++ stonesOnTheTable.cpp
@@ -0,0 +1,24 @@
+#define ll long long 
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    // your code goes here
+
+   bool check=true;
+   ll n;cin>>n;
+   string s;
+   cin>>s;
+   int cnt=0;
+   for(int i=1;i<n;i++){
+    if(s[i]==s[i-1]){
+        cnt++;
+
+    }
+   }cout<<cnt<<endl;
+   
+    
+    
+ 
+}
