7393e72f3420aecae713c34cb5e73381e5f0b03b
Dhruv Gupta <dhruv.24bce10799@vitbhopal.ac.in>
2026-07-22 20:41:15 +0530

    Create georgeAndAccommodation.cpp
    Day 11 - George And Accommodation

---
+++ georgeAndAcoomodation.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    int ans = 0;
+
+    while (n--) {
+        int p, q;
+        cin >> p >> q;
+
+        if (q - p >= 2)
+            ans++;
+    }
+
+    cout << ans << endl;
+
+    return 0;
+}
