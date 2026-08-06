3070346bf218566c88e552e0496a0a9023ac90f1
Dhruv Gupta <dhruv.24bce10799@vitbhopal.ac.in>
2026-08-06 18:50:11 +0530

    Create bersuBall.cpp

---
+++ bersuBall.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    for (int i = 0; i < n; i++) cin >> a[i];
+    int m;
+    cin >> m;
+    vector<int> b(m);
+    for (int i = 0; i < m; i++) cin >> b[i];
+    sort(a.begin(), a.end());
+    sort(b.begin(), b.end());
+    int i = 0, j = 0, ans = 0;
+    while (i < n && j < m) {
+        if (abs(a[i] - b[j]) <= 1) {
+            ans++;
+            i++;
+            j++;
+        } else if (a[i] < b[j]) {
+            i++;
+        } else {
+            j++;
+        }
+    }
+    cout << ans << "\n";
+    return 0;
+}
